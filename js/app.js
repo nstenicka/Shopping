350e7324b642da45b6e92867498efe782dcd0df5
nstenicka <natashastenicka@ymail.com>
2016-08-05 12:38:23 -0500

    centering list items

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-// document ready before functions load                                                                                                              
-$(document).ready(function(){                                                                                                                                                                                                              
-    $('#add-items').keydown(function(event){                                                                                           
-        if(event.keyCode == 13) {                                                                                                              
-            var text = $('#add-items').val();                                                                                               
-            var work = '<p class="full-width no-strikethrough">' + text + '</p>';                                                                                                                  
-            $('#list-area').prepend(work);                                                                                                        
+// wait until the document is ready                                                                                                               
+$(document).ready(function(){                                                                                                                     
+    // listen to the events, when keys are pressed down                                                                                           
+    $('#add-items').keydown(function(event){                                                                                                      
+                                                                                                                                                  
+        // check if the key pressed, was the enter key                                                                                            
+        if(event.keyCode == 13) {                                                                                                                 
+            // get value from input                                                                                                               
+            var text = $('#add-items').val();                                                                                                     
+            // create new item list for addition                                                                                                  
+            var work = '<ul class="full-width no-strikethrough">' + text + '</ul>';                                                                 
+            // add to the list                                                                                                                    
+            $('#list-area').prepend(work); 
+			$('add-items').val('');                                                                                                        
         }                                                                                                                                         
-    });                                                                                                                                                                                                                                                                                         
+    });                                                                                                                                           
+                                                                                                                                                  
 });
